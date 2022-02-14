648997e4f8fa3517612c12bac0727fda830816f7
Sujit Rajbanshi <61191607+SujitRajbanshi5@users.noreply.github.com>
2022-02-14 19:49:31 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value,
+    };
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
+
     showNotes();
 });
 
@@ -27,8 +34,8 @@ function showNotes() {
         html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title"> ${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
         </div>`;
@@ -42,7 +49,6 @@ function showNotes() {
 }
 
 function deleteNote(index) {
-    // console.log("i am deleting")
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
@@ -56,8 +62,8 @@ function deleteNote(index) {
 
 let search = document.getElementById("searchTxt");
 search.addEventListener("input", function() {
-
     let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName("noteCard");
     Array.from(noteCards).forEach(function(element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
@@ -66,5 +72,6 @@ search.addEventListener("input", function() {
         } else {
             element.style.display = "none";
         }
-    })
-})
+        // console.log(cardTxt);
+    });
+});
